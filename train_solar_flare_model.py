5e16c404d95ebfa90da7d721c4ec51f7d8a15d69
rrocketter <rahul.rocket711@gmail.com>
2025-03-09 15:19:49 -0400

    Update train_solar_flare_model.py

--- train_solar_flare_model.py
+++ train_solar_flare_model.py
@@ -368,15 +368,15 @@ class SolarFlarePredictor:
             make_prior_fn=make_prior_fn,
             # make_posterior_fn=tfpl.util.default_mean_field_normal_fn(),
             # make_posterior_fn=default_mean_field_normal_fn(),
-            make_posterior_fn=tfp.layers.default_mean_field_normal_fn(
-                                is_singular=False,
-                                loc_initializer=tf.random_normal_initializer(stddev=0.1),
-                                untransformed_scale_initializer=tf.random_normal_initializer(mean=-3.0, stddev=0.1),
-                                loc_regularizer=None,
-                                untransformed_scale_regularizer=None,
-                                loc_constraint=None,
-                                untransformed_scale_constraint=None
-                            ),
+            make_posterior_fn=lambda kernel_size, bias_size, dtype, *args: tfp.layers.default_mean_field_normal_fn(
+                is_singular=False,
+                loc_initializer=tf.random_normal_initializer(stddev=0.1),
+                untransformed_scale_initializer=tf.random_normal_initializer(mean=-3.0, stddev=0.1),
+                loc_regularizer=None,
+                untransformed_scale_regularizer=None,
+                loc_constraint=None,
+                untransformed_scale_constraint=None
+            )(kernel_size, bias_size, dtype),
             kl_weight=1 / self.config['batch_size'],
             activation=None
         )(x)
