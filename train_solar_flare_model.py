7bb5a848b3ee89f044ccfac755ca3102c0d98406
rrocketter <rahul.rocket711@gmail.com>
2025-03-08 23:42:44 -0500

    Update train_solar_flare_model.py

--- train_solar_flare_model.py
+++ train_solar_flare_model.py
@@ -359,7 +359,9 @@ class SolarFlarePredictor:
             units=tfpl.IndependentNormal.params_size(output_dims),
             make_prior_fn=lambda *args, **kwargs: prior,
             # make_posterior_fn=tfpl.util.default_mean_field_normal_fn(),
-            make_posterior_fn=default_mean_field_normal_fn(),
+            # make_posterior_fn=default_mean_field_normal_fn(),
+            make_posterior_fn=lambda *args, **kwargs: default_mean_field_normal_fn()(trainable=True,
+                                                                                     add_variable_fn=tf.keras.backend.variable),
             kl_weight=1 / self.config['batch_size'],
             activation=None
         )(x)
