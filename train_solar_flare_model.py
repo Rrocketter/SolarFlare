c0315cf52c5632e749db5a5b52e0d21375c3d152
rrocketter <rahul.rocket711@gmail.com>
2025-03-09 21:59:38 -0400

    Update train_solar_flare_model.py

--- train_solar_flare_model.py
+++ train_solar_flare_model.py
@@ -358,13 +358,15 @@ class SolarFlarePredictor:
                            ), reinterpreted_batch_ndims=1)
 
         make_posterior_fn = tfp.layers.default_mean_field_normal_fn(
+            trainable=True,  # Explicitly added
+            add_variable_fn=tfp.layers.default_add_variable_fn,  # Explicitly added
             is_singular=False,
             loc_initializer=tf.random_normal_initializer(stddev=0.1),
             untransformed_scale_initializer=tf.random_normal_initializer(mean=-3.0, stddev=0.1),
             loc_regularizer=None,
             untransformed_scale_regularizer=None,
             loc_constraint=None,
-            untransformed_scale_constraint = None
+            untransformed_scale_constraint=None
         )
 
         # Variational posterior with trainable parameters
