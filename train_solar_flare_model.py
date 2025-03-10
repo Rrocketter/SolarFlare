a955cef2e7e35cad84b6d518472c3ab8b3c66846
rrocketter <rahul.rocket711@gmail.com>
2025-03-09 20:50:28 -0400

    Update train_solar_flare_model.py

--- train_solar_flare_model.py
+++ train_solar_flare_model.py
@@ -359,8 +359,8 @@ class SolarFlarePredictor:
 
         make_posterior_fn = tfp.layers.default_mean_field_normal_fn(
             is_singular=False,
-            loc_initializer=tf.randon_nornal_initializer(stddev=0.1),
-            untransformed_scale_initializer=tf.random_normal_initializer(nean=-3.0, stddev=0.1),
+            loc_initializer=tf.random_normal_initializer(stddev=0.1),
+            untransformed_scale_initializer=tf.random_normal_initializer(mean=-3.0, stddev=0.1),
             loc_regularizer=None,
             untransformed_scale_regularizer=None,
             loc_constraint=None,
